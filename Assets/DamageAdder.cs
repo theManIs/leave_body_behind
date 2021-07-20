85d4d7819a42fdb9c52b265bde36519e1d157fc0
Jim Raynor <lostlose@yandex.ru>
2021-07-20 23:06:43 +0300

    Добавлено недвижение в воздухе и смерть.

---
+++ Assets/DamageAdder.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageAdder : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D c)
+    {
+        if (!c.isTrigger)
+        {
+            Debug.Log(c.gameObject.name);
+
+            BrainAnimationController animco = c.gameObject.GetComponent<BrainAnimationController>();
+
+            if (animco)
+            {
+                animco.Die();
+            }
+
+            BrainCharacterController brain = c.gameObject.GetComponent<BrainCharacterController>();
+
+            if (brain)
+            {
+                brain.DisableCharacter();
+            }
+        }
+
+    }
+}
